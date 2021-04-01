fc136598e193451601be6fb86fb287184dcf8e4b
Hasanboy Marufjonov <theRealHasanboy@gmail.com>
2021-04-01 16:42:51 +0500

    improved

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-
+/*
 const fname = document.querySelector('#fname');
 const lname = document.querySelector('#lname');
 const message = document.querySelector('#subject');
 const msg = document.querySelector('.msg');
-const button = document.querySelector('.btn');
+const button = document.querySelector('.btn'); 
 
 button.addEventListener('click', (e) =>{
     e.preventDefault();
@@ -20,9 +20,45 @@ button.addEventListener('click', (e) =>{
         emailInput.value = '';
      }
 });
-
-
+*/
+/*
 button.addEventListener('click', (e)=> {
     e.preventDefault();
     console.log('button clicked')
 })
+*/
+
+
+var x = document.forms["myForm"]["firstname"].value;
+var y = document.forms["myForm"]["lastname"].value;
+var z = document.forms["myForm"]["subject"].value;
+/*
+function validateForm(){
+    if (x === ""){
+        alert("You didn't enter your firstname");
+        return false;
+    }
+}
+
+function validateForm(){
+    if (y === ""){
+        alert("You didn't enter your lastname");
+        return false;
+    }
+}
+
+function validateForm(){
+    if (z === ""){
+        alert("You didn't enter your message");
+        return false;
+    }
+}
+*/
+
+
+function validateForm(){
+    if (x === '' || y === '' || z === ''){
+        alert("Form must be full filled out");
+        return false;
+    }
+}
